11a6e18829fe8999b3be7069409aea60cc4806b1
alessandroliafook <alessandro.liafook@gmail.com>
2016-11-25 00:03:12 -0300

    Learning Html, Css, JS, Angulas

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Alessandro Lia Fook Santos on 25/11/2016.
+ */
+
+var app = angular.module('myApp', []);
+
+app.controller('controller', function ($scope) {
+    $scope.firstName = "";
+    $scope.lastName= "";
+});
