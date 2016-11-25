ab4874b0537077d109dba0439a66c52598506a2c
alessandroliafook <alessandro.liafook@gmail.com>
2016-11-25 15:03:38 -0300

    Learning Html, Css, JS, Angulas

--- js/app.js
+++ js/app.js
@@ -7,4 +7,19 @@ var app = angular.module('myApp', []);
 app.controller('controller', function ($scope) {
     $scope.firstName = "";
     $scope.lastName= "";
+
+    $scope.taskList = new Array();
+    $scope.name = "";
+
+    var task = function (taskName) {
+      this.nome = taskName;
+      this.isDone = false;
+
+    };
+
+    $scope.addTask = function () {
+        $scope.taskList.push(new task($scope.name));
+        console.log($scope.name + " adicionada!");
+    };
+
 });
